826982f5f0f11fa5c6e95eca457caba9435e0b8f
MaddalenaRen <ileniarennella@gmail.com>
2025-03-14 16:45:22 +0100

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const logoNetflix = document.getElementById("logoNetflix");
+const navbarNav = document.getElementById("navbarNav");
+
+logoNetflix.addEventListener("click", function () {
+    navbarNav.classList.toggle("collapse");
+});
